8c3442224e2a798c8ba1847d1395d44f6bc31ab8
Michal Makowski <michmakow@gmail.com>
2018-04-21 11:40:10 +0200

    Fix userController

--- src/main/java/views/UserView.java
+++ src/main/java/views/UserView.java
@@ -16,6 +16,20 @@ public class UserView {
         System.out.println("Witaj " + user.getLogin());
     }
 
+    public static void giveName(){
+        System.out.println("Podaj imie");
+    }
+    public static void giveSurname(){
+        System.out.println("Podaj nazwisko");
+    }
+    public static void giveLogin(){
+        System.out.println("Podaj login");
+    }
+    public static void givePassword(){
+        System.out.println("Podaj haslo");
+    }
+
+
 //    public static void printAllUsers(List<User> users){
 //        for (user : users) {
 //            System.out.println(user);
