7d56954064cd936e07ba25ce5113ec816472dd04
Bartosz Smok <bsmok2@gmail.com>
2018-04-21 11:56:10 +0200

    initial tests

---
+++ src/test/java/UserTests.java
@@ -0,0 +1,15 @@
+import models.User;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UserTests {
+
+
+    @Test
+    public void shouldNotBeNullForNewUser(){
+        User user = new User();
+
+        assertThat(user).isNotNull();
+    }
+}
