af555b4544ead2aee22d0dbfdcaa443c4e2f8ee0
Bartosz Smok <bsmok2@gmail.com>
2018-04-21 15:11:40 +0200

    main init

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,19 @@
-import controllers.UserController;
-
 public class Main {
-    public static void main(String[] args) {
-        UserController uc = new UserController();
+    public enum State {
+        INIT,
+        LOGGING_IN,
+        LOGGED_IN,
+        EXIT
+    }
 
-        uc.addUser();
+    public static void main(String[] args) {
+        State state = State.INIT;
+
+        while (state != State.EXIT){
+            switch (state){
+                case INIT:
+                    
+            }
+        }
     }
 }
