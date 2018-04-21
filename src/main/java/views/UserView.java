8d10942bf0b3a667ee499d22f6d5ee2f7cdb3b86
wojciech wojtacha/woyt <wwojtacha@o2.pl>
2018-04-21 11:55:43 +0200

    PrintAllUsers added

--- src/main/java/views/UserView.java
+++ src/main/java/views/UserView.java
@@ -1,7 +1,7 @@
 package views;
 
 
-import controllers.UserController;
+
 import models.User;
 
 import java.util.List;
@@ -21,32 +21,27 @@ public class UserView {
         System.out.println("Witaj " + user.getLogin());
     }
 
-<<<<<<< HEAD
     public static void printAllUsers(List<User> users) {
 
         for (User user : users) {
             System.out.println(user.getLogin());
         }
     }
-=======
-    public static void giveName(){
+
+    public static void giveName() {
         System.out.println("Podaj imie");
     }
-    public static void giveSurname(){
+
+    public static void giveSurname() {
         System.out.println("Podaj nazwisko");
     }
-    public static void giveLogin(){
+
+    public static void giveLogin() {
         System.out.println("Podaj login");
     }
-    public static void givePassword(){
+
+    public static void givePassword() {
         System.out.println("Podaj haslo");
     }
 
-
-//    public static void printAllUsers(List<User> users){
-//        for (user : users) {
-//            System.out.println(user);
-//
-//        }
->>>>>>> ab527cc30a01c191f28d37fa92af077c1b6207de
-}
+    }
