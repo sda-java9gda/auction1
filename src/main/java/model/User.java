c2263cdc401b23e880bea0a2c4b8ce8eb988c3f0
Michal Makowski <michmakow@gmail.com>
2018-04-21 11:10:45 +0200

    UserControler methods

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,10 +1,13 @@
-package Model;
+package model;
 
 public class User {
     private String name;
     private String surname;
     private String login;
     private String password;
+
+    public User() {
+    }
 
     public User(String name, String surname, String login, String password) {
         this.name = name;
