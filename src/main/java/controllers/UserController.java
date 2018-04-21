93200131c238edd28fb2ce0523f227b3489a31a4
wojciech wojtacha/woyt <wwojtacha@o2.pl>
2018-04-21 12:55:10 +0200

    file writer

--- src/main/java/controllers/UserController.java
+++ src/main/java/controllers/UserController.java
@@ -52,9 +52,9 @@ public class UserController {
         } else {
             user.setLogin(login);
             giveUserPassword();
+            FileController.writeToUsersFile(login);
             return false;
         }
-        FileController.writeToUsersFile(login);
     }
 
     public void giveUserPassword() {
