741971f37a184797a6e75c09c2606e9efe895dee
wojciech wojtacha/woyt <wwojtacha@o2.pl>
2018-04-22 10:51:56 +0200

    auction clear

--- src/main/java/models/Auction.java
+++ src/main/java/models/Auction.java
@@ -25,45 +25,19 @@ public class Auction {
         this.settingUser = settingUser;
     }
 
-    public String getName() {
-        return name;
-    }
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
     }
 
     public void setDescription(String description) {
         this.description = description;
     }
 
-    public User getSettingUser() {
-        return settingUser;
-    }
-
     public void setSettingUser(User settingUser) {
         this.settingUser = settingUser;
     }
 
-    public User getBiddingUser() {
-        return biddingUser;
-    }
-
-    public void setBiddingUser(User biddingUser) {
-        this.biddingUser = biddingUser;
-    }
-
-    public int getNumberOfBiddings() {
-        return numberOfBiddings;
-    }
-
-    public void setNumberOfBiddings(int numberOfBiddings) {
-        this.numberOfBiddings = numberOfBiddings;
-    }
 
     @Override
     public String toString() {
