6f6336e150aa538f83cc314e6f6b9e052f285062
Bartosz Smok <bsmok2@gmail.com>
2018-04-28 11:13:21 +0200

    initial tests commit

--- src/main/java/controllers/UserController.java
+++ src/main/java/controllers/UserController.java
@@ -20,11 +20,15 @@ public class UserController {
 
     public void addUser(String login, String password) {
         user = new User(login, password);
+
+        //to nie jest kontroler - w userze
         String input = fc.toLine(user);
         FileController.writeToUsersFile(input, PATHNAME);
     }
 
     public boolean checkIfLoginPresent(String string, Map<String, User> users) {
+
+        //contains
         for (String login : users.keySet()) {
             if (login.equals(string)) {
                 return true;
@@ -38,6 +42,8 @@ public class UserController {
     }
 
     public void verify(String login, String password, Map<String, User> users) throws NoSuchUserException, WrongPasswordException {
+
+        //zmienic na jeden wyjatek, zeby nie bylo widac, ze konto istnieje
        if(!checkIfLoginPresent(login,users)){
            throw new NoSuchUserException();
        }
